4f84d756b8d21095bc61c03fd08921a946e9000a
Alysson Cotta Paes <alyssonpaes@hotmail.com>
2026-02-08 19:33:19 -0300

    criando casos de teste positivos e negativos e criando objeto de seletores para facilitar futuras alterações

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,22 +1,32 @@
 describe('Orange HRM Tests', () => {
+
+  //Objeto de seletores para facilitar a alteração dos campos que se repetem
+  const selectorList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    buttonField: ".oxd-button",
+    wrongCredentialAlert: ".oxd-alert",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text"
+  }
+        
   //cenário de sucesso
   it('Login - Sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Admin')
-    cy.get('[name="password"]').type('admin123')
-    cy.get('.oxd-button').click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.buttonField).click()
+    //cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     // outro jeito de garantir a url correta 
     // cy.url().should('equal', 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
   })
 
   //cenário de falha
-  it.only('Login - fail', () => {
+  it('Login - fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('wrong-user')
-    cy.get('[name="password"]').type('wrong-sauce')
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-alert')
+    cy.get(selectorList.usernameField).type('wrong-user')
+    cy.get(selectorList.passwordField).type('wrong-sauce')
+    cy.get(selectorList.buttonField).click()
+    cy.get(selectorList.wrongCredentialAlert)
   })
 })  
