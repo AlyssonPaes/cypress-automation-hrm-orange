2f0e5b51b881c06e1d102104f159775ea5bd6b62
Alysson Cotta Paes <alyssonpaes@hotmail.com>
2026-02-12 09:38:58 -0300

    adding user data json

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,31 +1,34 @@
+import userData from '../fixtures/users/user-date.js'
+
 describe('Orange HRM Tests', () => {
 
-  //Objeto de seletores para facilitar a alteração dos campos que se repetem
+  //Objeto de seletores para facilitar a alteração dos campos (get) que se repetem
   const selectorList = {
     usernameField: "[name='username']",
     passwordField: "[name='password']",
     buttonField: ".oxd-button",
+    dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialAlert: ".oxd-alert",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text"
+    //sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text"
   }
         
   //cenário de sucesso
   it('Login - Sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type('Admin')
-    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.usernameField).type(userData.userSucess.username)
+    cy.get(selectorList.passwordField).type(userData.userSucess.password)
     cy.get(selectorList.buttonField).click()
-    //cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     // outro jeito de garantir a url correta 
-    // cy.url().should('equal', 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
-    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+    //cy.url().should('equal', 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+    //cy.get(selectorList.dashboardGrid).contains('Dashboard')
   })
 
   //cenário de falha
   it('Login - fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type('wrong-user')
-    cy.get(selectorList.passwordField).type('wrong-sauce')
+    cy.get(selectorList.usernameField).type(userData.userFail.username)
+    cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.buttonField).click()
     cy.get(selectorList.wrongCredentialAlert)
   })
